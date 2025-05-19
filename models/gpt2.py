86159bc22d8aea7a2ce63d84e07b0b5b93b0a354
Dahee Lee <dlxkgml@gmail.com>
2025-05-19 13:13:39 +0900

    Fix: raise 제거

--- models/gpt2.py
+++ models/gpt2.py
@@ -70,7 +70,7 @@ class GPT2Model(GPTPreTrainedModel):
     ### TODO: pos_ids를 사용하여 self.pos_embedding에서 위치 임베딩을 가져와 pos_embeds에 저장한다.
     ###       그런 다음, 두 개의 임베딩을 더하고, 드롭아웃을 적용한 뒤 반환한다.
     ### 완성시켜야 할 빈 코드 블록
-    raise NotImplementedError
+    
 
 
   def encode(self, hidden_states, attention_mask):
